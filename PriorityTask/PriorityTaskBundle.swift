b94420e49ed4aa2fea616556b6d6fd472aba79b4
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-12-03 11:26:58 +0100

    Added Widget Implementation

---
+++ PriorityTask/PriorityTaskBundle.swift
@@ -0,0 +1,17 @@
+//
+//  PriorityTaskBundle.swift
+//  PriorityTask
+//
+//  Created by Leo A.Molina on 03/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PriorityTaskBundle: WidgetBundle {
+    var body: some Widget {
+        PriorityTask()
+        PriorityTaskLiveActivity()
+    }
+}
---
+++ PriorityTask/PriorityTaskLiveActivity.swift
@@ -0,0 +1,80 @@
+//
+//  PriorityTaskLiveActivity.swift
+//  PriorityTask
+//
+//  Created by Leo A.Molina on 03/12/25.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct PriorityTaskAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var emoji: String
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct PriorityTaskLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: PriorityTaskAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello \(context.state.emoji)")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom \(context.state.emoji)")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T \(context.state.emoji)")
+            } minimal: {
+                Text(context.state.emoji)
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+extension PriorityTaskAttributes {
+    fileprivate static var preview: PriorityTaskAttributes {
+        PriorityTaskAttributes(name: "World")
+    }
+}
+
+extension PriorityTaskAttributes.ContentState {
+    fileprivate static var smiley: PriorityTaskAttributes.ContentState {
+        PriorityTaskAttributes.ContentState(emoji: "😀")
+     }
+     
+     fileprivate static var starEyes: PriorityTaskAttributes.ContentState {
+         PriorityTaskAttributes.ContentState(emoji: "🤩")
+     }
+}
+
+#Preview("Notification", as: .content, using: PriorityTaskAttributes.preview) {
+   PriorityTaskLiveActivity()
+} contentStates: {
+    PriorityTaskAttributes.ContentState.smiley
+    PriorityTaskAttributes.ContentState.starEyes
+}
