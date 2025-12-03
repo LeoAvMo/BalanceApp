0e0f95b6f05e10f6787beda33dcd67a46f394b9c
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-12-03 10:52:06 +0100

    Initial Commit

---
+++ BalanceApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BalanceApp
+//
+//  Created by Leo A.Molina on 03/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
