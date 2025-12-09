bbe4692ff6bfc573b857388059870213799e8931
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-12-09 10:14:04 +0100

    Added ToDoTask initializer and changed timeToComplete to TimeInterval data type.

---
+++ BalanceApp/ToDoTask.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoTask.swift
+//  BalanceApp
+//
+//  Created by Leo A.Molina on 04/12/25.
+//
+
+import SwiftData
+import SwiftUI
+
+@Model
+final class ToDoTask {
+    var name: String
+    var priority: Priority
+    var dueDate: Date
+    var timeToComplete: TimeInterval
+    
+    init(name: String = "", priority: Priority = .low, dueDate: Date = .now, timeToComplete: TimeInterval = 0.0) {
+        self.name = name
+        self.priority = priority
+        self.dueDate = dueDate
+        self.timeToComplete = timeToComplete
+    }
+}
+
+enum Priority {
+    case low, medium, high
+    
+    func toInt() -> Int {
+        switch self {
+            case .low: return 1
+            case .medium: return 2
+            case .high: return 3
+        }
+    }
+}
